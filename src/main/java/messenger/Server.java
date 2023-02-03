b9ec60c75651c4901f6a47f3abd7b414f5f3b9a3
Yeongwoo Hwang <yeongwoo@gmail.com>
2023-02-03 16:57:44 -0500

    added test structure

--- src/main/java/messenger/Server.java
+++ src/main/java/messenger/Server.java
@@ -1,4 +1,4 @@
-package src.main.java.Messenger;
+package messenger;
 
 import java.io.*;
 import java.net.*;  
---
+++ src/test/java/messenger/ClientUnitTests.java
@@ -0,0 +1,22 @@
+package messenger;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+class ClientUnitTests {
+    @Test
+    void testList(){
+        System.out.println("This test method should be run");
+        List mockedList = Mockito.mock(List.class);
+        // or even simpler with Mockito 4.10.0+
+        // List mockedList = mock();
+
+        // using mock object - it does not throw any "unexpected interaction" exception
+        mockedList.add("one");
+        mockedList.clear();
+
+        // selective, explicit, highly readable verification
+        Mockito.verify(mockedList).add("one");
+        Mockito.verify(mockedList).clear();
+    }
+}
