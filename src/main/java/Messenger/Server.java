77dcdb623e21b9f15d6d9499fb729be0420e04d2
Cassandra <cassandramarcussencello@gmail.com>
2023-02-03 17:02:50 -0500

    Adding options message to server

--- src/main/java/Messenger/Server.java
+++ src/main/java/Messenger/Server.java
@@ -17,6 +17,19 @@ public class Server {
             ServerSocket ss=new ServerSocket(6666);
             System.out.println("Waiting for connection...");
             Socket s=ss.accept();//establishes connection
+
+            String options = "Pick a method:\n" +
+            "1. Create an account. You must supply a unique user name.\n" +
+            "2. List accounts (or a subset of the accounts, by text wildcard)\n" +
+            "3. Send a message to a recipient.\n" +
+            "4. Deliver undelivered messages to a particular user.\n" +
+            "5. Delete an account. If you attempt to delete an account that contains undelivered message, (ADD HERE)";
+
+            while(true) {
+                System.out.println(options);
+                break;
+            }
+
             DataInputStream dis=new DataInputStream(s.getInputStream());
             String  str=(String)dis.readUTF();
             System.out.println("message= "+str);
