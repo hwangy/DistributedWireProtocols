46bc4243406c5c59624c6abfc69d332867ded1fb
Cassandra <cassandramarcussencello@gmail.com>
2023-02-14 10:45:32 -0500

    Adding content to ClientCore

--- src/main/java/messenger/ClientCore.java
+++ src/main/java/messenger/ClientCore.java
@@ -1,6 +1,29 @@
 package messenger;
 
+import messenger.objects.Message;
+import messenger.objects.request.*;
+import messenger.objects.response.*;
+
+import java.util.*;
+
 public class ClientCore {
+    private String username;
+
     public ClientCore() {
+       this.username = "";
     }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public LoginResponse setUsername(String username) {
+        
+    }
+
+    // actually should be in server?
+    public LoginResponse setLoginUsername(LoginRequest request) {
+        String username = request.getUsername();
+    }
+
 }
